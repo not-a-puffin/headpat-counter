4c022005185309e2fce046d93d4628d44593c05b
not-a-puffin <128986127+not-a-puffin@users.noreply.github.com>
2026-01-08 01:28:25 -0500

    update command

--- cmd/subs_start.sh
+++ cmd/subs_start.sh
@@ -8,4 +8,10 @@ curl -w "\n" -X POST 'https://api.twitch.tv/helix/eventsub/subscriptions' \
 -H 'Authorization: Bearer '$APP_ACCESS_TOKEN'' \
 -H 'Client-Id: '$APP_CLIENT_ID'' \
 -H 'Content-Type: application/json' \
--d '{"type":"channel.channel_points_custom_reward_redemption.add","version":"1","condition":{"broadcaster_user_id":"'$BROADCASTER_USER_ID'", "reward_id":"'$REWARD_ID'"},"transport":{"method":"webhook","callback":"'$BASE_URL'/headpat/notification","secret":"'$WEBHOOK_SECRET'"}}'
+-d '{"type":"channel.channel_points_custom_reward_redemption.add","version":"1","condition":{"broadcaster_user_id":"'$BROADCASTER_USER_ID'", "reward_id":"'$REWARD_ID'"},"transport":{"method":"webhook","callback":"'$BASE_URL'/notification/headpat","secret":"'$WEBHOOK_SECRET'"}}'
+
+curl -w "\n" -X POST 'https://api.twitch.tv/helix/eventsub/subscriptions' \
+-H 'Authorization: Bearer '$APP_ACCESS_TOKEN'' \
+-H 'Client-Id: '$APP_CLIENT_ID'' \
+-H 'Content-Type: application/json' \
+-d '{"type":"stream.online","version":"1","condition":{"broadcaster_user_id":"'$BROADCASTER_USER_ID'"},"transport":{"method":"webhook","callback":"'$BASE_URL'/notification/online","secret":"'$WEBHOOK_SECRET'"}}'
